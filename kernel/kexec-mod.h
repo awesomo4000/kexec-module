40bddb05e6f0aaea81f402222fa5dc90f3555737
awesomo4000 <alrhodes@gmail.com>
2018-10-18 02:02:41 -0400

    Add version magic

--- kernel/kexec-mod.h
+++ kernel/kexec-mod.h
@@ -2,3 +2,6 @@
 #include <linux/kexec.h>
 #undef  VMCOREINFO_SYMBOL
 #define VMCOREINFO_SYMBOL(_) do {} while (0)
+
+MODULE_INFO(vermagic, "3.18.48LE.UM.1.2-29600-9x07-136-g7f63904 preempt mod_unload ARMv7 p2v8 ");
+MODULE_LICENSE("GPL");
